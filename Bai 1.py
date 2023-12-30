41a2ca6f1986da7b404fef24653c535e1c1c61a5
thuvan-tr <154805426+thuvan-tr@users.noreply.github.com>
2023-12-30 19:05:26 +0700

    Bài TH số 1

---
+++ Bai 1.py
@@ -0,0 +1,19 @@
+#Bai 1
+print("Hello world")
+
+#Bai 2
+a="hello world"
+print(a)
+
+#Bai 3
+b="hello"
+c=" "
+d="world"
+e=b+c+d
+print(e)
+
+#Bai 4
+def swift():
+    print(e)
+    return;
+swift()
